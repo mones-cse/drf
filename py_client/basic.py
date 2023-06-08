a0c00998d4a56c0af6bf3dbf834275c2a2e8cda8
mones <mones.cse@gmail.com>
2023-06-08 10:55:13 +0600

    called to django endpoint

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,12 +3,17 @@ import requests
 print('Hello World!')
 
 endpoint = "https://httpbin.org/anything"
-endpoint2 = "http://httpbin.org/status/200"
+endpoint = "http://httpbin.org/status/200"
+endpoint = "http://localhost:8000"
 
 get_response = requests.get(endpoint, json={"key": "value"})
-print(get_response.json())
+print(get_response.text)
+print(get_response.status_code)
 # {...'data': '{"key": "value"}' ...}
 
-get_response = requests.get(endpoint, data={"key": "value"})
-print(get_response.json())
+# get_response = requests.get(endpoint, data={"key": "value"})
+# print(get_response.json())
 # {...'form': '{"key": "value"}' ...}
+
+
+# https://youtu.be/c708Nf0cHrs?t=1648
