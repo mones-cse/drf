4dd7edd794e0e11a3b1a2de77708e705e5962db8
mones <mones.cse@gmail.com>
2023-06-08 14:11:49 +0600

    added rest_framework and use Response and api_view

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,11 +2,22 @@ import json
 from django.http import JsonResponse
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def api_home(request):
+    product_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if product_data:
+        data = model_to_dict(product_data, fields=['id', 'title', 'price'])
+    return Response(data)
+
+
+def product_with_django_forms_models(request):
     product_data = Product.objects.all().order_by("?").first()
     data = {}
     if product_data:
